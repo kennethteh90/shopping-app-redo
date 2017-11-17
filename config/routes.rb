1eb9ef4c0969cba9c4c1e99c9898fb526614863b
kennethteh90 <kennethteh90@gmail.com>
2017-11-17 23:24:26 +0800

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+  namespace :admin do
+    root to: 'products#index'
+    resources :products, only: [:new, :create, :edit, :update, :destroy]
+    resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
+  end
+
+
 end
